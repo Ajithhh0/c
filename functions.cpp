9905e5c7c5cdd2f81ee63c9952605ac7084c1c5c
Ajithhh <ajithvn01@gmail.com>
2022-09-13 17:02:39 +0530

    idk

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b){
+    return(a+b);
+
+}
+int main(){
+ int sum;
+
+ sum = add(100,200);
+
+ cout<<"sum: "<<sum<<endl;
+    
+ 
+
+    
+
+    return 0;
+
+
+}
