d6e130bb5aaa820f3d7632aaa38f388377902e60
Ajithhh <ajithvn01@gmail.com>
2022-09-13 17:10:10 +0530

    h

---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+
+    double num, sqrtn;
+     cout<<"Enter num : "<<endl;
+     cin>>num;  
+
+     sqrtn = sqrt(num);
+
+     cout<<" Square root : "<<sqrtn;
+
+     return 0;
+}
